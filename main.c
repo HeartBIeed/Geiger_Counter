7da38af8e048f7f82b9e225e0630c27771f8a09c
Дмитрий <starikov.dmitriy@yandex.ru>
2025-10-31 00:01:48 +0300

    counter

--- main.c
+++ main.c
@@ -2,10 +2,11 @@
 
 
 volatile uint32_t ms_cnt= 0;// счетчик ms
-volatile uint32_t gamma_cnt = 0; // счет событий
 volatile uint8_t gamma_fact = 0; //флаг события
 
-
+uint8_t gamma_array[5]={0};
+uint8_t index_garray = 0;
+uint8_t index_gbit = 0;
 
 //------------- timer 0 for mills -------------------
 
@@ -30,30 +31,46 @@ uint32_t get_mills()
 //-------------------- gamma ----------------------
 ISR(INT0_vect) // внешнее прерывание PD2
 {
-	gamma_cnt++;
 	gamma_fact =1;
+	gamma_array[index_garray] |= (1 << index_gbit);
 }
 
+//****************************************************** */
+int num_ones(unsigned char array[])
+{    
+int counter =0;
+		for(int ind_arr = 0;ind_arr<5;ind_arr++)
+			{
+			for(int ind_pos = 0;ind_pos<8;ind_pos++)
+				{
+				
+					if (array[ind_arr] & (1 <<ind_pos))
+						{
+							counter++;
+						}
+				}
+			}
+	return counter;
+}
+//********************************************************
 
 int main(void)
 {
 	
-uint8_t gamma_array[4]={0};
-uint8_t index_garray = 0;
-uint8_t index_gbit = 0;
+
 
 	sei();
 	timer_ini();
 	i2c_init();		
 
 	LCD1602_ini();
-	USART_init(103);
+	USART_init(103); //9600
 
 	USART_send_str("UART EN");
 	
 //------------------------ interrupt--------------
 
-	MCUCR  = /*(1<<ISC00)|*/(1<<ISC01); // falling INT0 
+	MCUCR  = (1<<ISC01); // falling INT0 
 	GICR |= (1<<INT0); // int0 enable (PD2)
 	
 //------------------------set ds1307-----------------
@@ -76,7 +93,7 @@ uint8_t index_gbit = 0;
 uint32_t start[3] = {0};
 uint32_t start_cnt = 0;
 
-char num_gm_cnt[4]="00/n";
+char num_gm_cnt[5]="00/n";
 
 
 while(1)
@@ -84,9 +101,9 @@ while(1)
 		
 //----------------time to lcd ----------------
 if (ms_cnt - start[1] >= 500)
-{		
-		time_to_lcd(0,0);
-		
+	{		
+		time_to_lcd(4,0);
+			
 		start[1] = get_mills();
 	}
 		
@@ -109,51 +126,59 @@ if (data_ready)
 */
 //--------------gamma-------------------
 
-	if (gamma_cnt - start_cnt >= 1)
-		{
 
-			set_lcd_pos(12,0);
-			sprintf(num_gm_cnt,"%d",gamma_cnt);
-			send_lcd_ptr_str(num_gm_cnt);
-			start_cnt = gamma_cnt;	
-		}
+if (gamma_fact)
+	{
+		set_lcd_pos(0,1);
+		send_lcd_char('G');
+
+		set_lcd_pos(5,1);
+		sprintf(num_gm_cnt,"%d",num_ones(gamma_array));
+		send_lcd_ptr_str(num_gm_cnt);
+
+		set_lcd_pos(7,1);
+		send_lcd_ptr_str("uR");
 
 
+		gamma_fact = 0; // сброс флага		
 
-	if (gamma_fact)
-		{
-			set_lcd_pos(11,0);
-			send_lcd_char('G');
-			
-		if (ms_cnt - start[0] >= 500)
-			{
-				set_lcd_pos(11,0);
-				send_lcd_char(' ');
-				gamma_fact = 0;
-				start[0] = get_mills();
-			}
-
-		}
+	}
 		
-	//********************************** */
+//********************************** */
 
-	if (ms_cnt - start[2] >= 1000)
-	{		
+if (ms_cnt - start[0] >= 1000)
+	{
+			set_lcd_pos(0,1);
+			send_lcd_char(' ');
+			start[0] = get_mills();
 
-		index_gbit++;
-
-			if (index_gbit>=8)
-			{		
-				index_garray ++;			
-				index_gbit = 0;
-					if (index_garray>=5)
-					index_garray =0;
-			}
-
-		gamma_array[index_garray]&= ~(1<<index_gbit);
 	}
 
 
+
+if (ms_cnt - start[2] >= 1000)
+	{	
+
+	start[2] = get_mills();
+	index_gbit++;
+
+	if (index_gbit>=8) //предел гамма битов значений массива
+		{	
+
+		index_garray ++;			
+		index_gbit = 0;
+
+			if (index_garray>=5) //предел гамма массива
+				{
+					index_garray =0;
+				}
+					
+		}
+
+	gamma_array[index_garray]&= ~(1<<index_gbit); //1 сек - обнуление бита из гамма массива
 	
 	}
+}
+
+	
 }
